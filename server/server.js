bbbf76c1fe762e113e330674ee973c3f1b36affb
jeanralphaviles <jeanralph.aviles@gmail.com>
2014-09-20 00:01:19 -0400

    Added startGame and endGame events to the server and index

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var clients = [];
 var connectedPlayers = 0;
 
 app.get('/', function(req, res){
@@ -9,10 +10,26 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     connectedPlayers = connectedPlayers + 1;
+    clients.push(socket);
     console.log('a user has connected');
+    if (connectedPlayers === 2) {
+      var randomClient = Math.floor(Math.random() * connectedPlayers);
+      // startGame === true -> first player
+      clients[randomClient].emit('startGame', true);
+      clients[randomClient === 0 ? 1 : 0].emit('startGame', false);
+      console.log('two players are now connected, game starting');
+    }
 
     socket.on('disconnect', function() {
       connectedPlayers = connectedPlayers - 1;
+
+      // Delete socket from clients[]
+      var i = array.indexOf(socket);
+      if(i != -1) {
+          array.splice(i, 1);
+      }
+
+      socket.broadcast.emit('endGame', '');
       console.log('a user has disconnected');
     });
 
